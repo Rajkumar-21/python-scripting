545d43dfb30aaa70ce86f12839a2bfa99151130b
Rajkumar@Jarvis <rajkumarravi21897@gmail.com>
2022-11-13 11:57:51 +0530

    cipher added - project

--- Projects/Caesar Cipher/Cipher.py
+++ Projects/Caesar Cipher/Cipher.py
@@ -1,3 +1,6 @@
+import startup_font
+print(startup_font.cipher)
+print(startup_font.Author)
 alphabet= ['a','b','c','d','e','f','g','h','i','j',
 'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j',
 'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
